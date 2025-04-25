09c16b95709bf504027dcb0aa0f94745b5252f36
Sharan <32306006+Sai1106175@users.noreply.github.com>
2025-04-25 16:24:53 +0530

    Create Final Variable.java

---
+++ Final Variable.java
@@ -0,0 +1,26 @@
+import java.security.PublicKey;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello Sai");
+        System.out.println("Hello Bhanu");
+        System.out.print("Sai");
+        System.out.println("Sai");
+        System.out.println("Sai");
+
+
+
+     int Mynumber = 20; // Declaration of Variable and assiging the value.
+     System.out.println(Mynumber); // Out put will be the same as mentioned.
+
+     int otherNumber = 100; // Declaration of Variable and assigned value.
+     otherNumber = 300; // Overriding the value to the variable name OtherNumber which is already assigned.
+     System.out.println(otherNumber); // The output will be Overided value.
+
+     
+
+    }
+}
+
+
