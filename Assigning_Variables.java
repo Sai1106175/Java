12fd713998839d661e6804eeb666b10265465979
Sharan <32306006+Sai1106175@users.noreply.github.com>
2025-04-28 12:30:04 +0530

    Create Assigning_Variables.java

---
+++ Assigning_Variables.java
@@ -0,0 +1,19 @@
+import java.security.PublicKey;
+
+public class Main {
+    public static void main(String[] args) {
+       String studentName = "Sai Sharan";
+       int StudentrollNo = 4893;
+       float studentWeight = 45.6f;
+       char areaZone = 'Z';
+       boolean wasThecandidateGreat = true;
+
+       System.out.println("Student Name = " + studentName);
+        System.out.println("Student Roll No  = " + StudentrollNo);
+        System.out.println("Student Weight = " + studentWeight);
+        System.out.println("Student Areazone = " + areaZone);
+
+    }
+}
+
+
