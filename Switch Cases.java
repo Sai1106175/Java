de096c19f63d04b9756d8d94ddd0777d7f9f5edf
Sharan <32306006+Sai1106175@users.noreply.github.com>
2025-05-07 10:46:33 +0530

    Create Switch Cases.java

---
+++ Switch Cases.java
@@ -0,0 +1,37 @@
+import java.security.PublicKey;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int day = 4;
+
+        switch (day) {
+            case 1:
+                System.out.println("Its going to be monday");
+                break;
+
+            case 2:
+                System.out.println("Its going to be Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Its going to be Wednesday");
+                break;
+
+            case 4:
+                System.out.println("Its going to be Thursday");
+                break;
+
+            case 5:
+                System.out.println("Its going to be Friday");
+                break;
+        }
+
+
+
+
+    }
+}
+
+
+
