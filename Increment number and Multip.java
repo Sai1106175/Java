6d090ef48fb57dc999edb78d21c23bd01ab242dd
Sharan <32306006+Sai1106175@users.noreply.github.com>
2025-05-12 15:18:21 +0530

    Create Increment number and Multip.java

---
+++ Increment number and Multip.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 10; i = i + 2) {
+            System.out.println(i);
+
+        }
+
+        for (int j = 1; j <= 10; j++) {
+            System.out.println("3 X " + j + "  = " + (3 * j)); // Multiplication.
+        }
+
+    }
+}
