c068a181aaa775f69819de1b94baccd9a96d411f
Sharan <32306006+Sai1106175@users.noreply.github.com>
2025-05-27 17:37:11 +0530

    Create Default Constructor.java

---
+++ Default Constructor.java
@@ -0,0 +1,12 @@
+public class Book{
+
+    public static void main(String[] arg){
+        Book bk = new Book();
+
+    }
+
+    Book(){
+        System.out.println("Book object created");
+    }
+
+}
