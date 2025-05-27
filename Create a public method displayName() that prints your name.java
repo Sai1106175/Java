0930e5a08f25347e43733dfb0093f7073a86f889
Sharan <32306006+Sai1106175@users.noreply.github.com>
2025-05-27 15:08:41 +0530

    Create Create a public method displayName() that prints your name.java

---
+++ Create a public method displayName() that prints your name.java
@@ -0,0 +1,13 @@
+/* Create a public method displayName() that prints your name */
+
+public class Main {
+    public void displayName(){
+        System.out.println("My Name is Sharan");
+    }
+
+    public static void main(String[] arg){
+        Main obj1 = new Main();
+        obj1.displayName();
+
+    }
+}
